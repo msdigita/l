46d56bd30b02ccc5025dc6cf4812c09f04c5ddab
msdigita <141251581+msdigita@users.noreply.github.com>
2023-08-07 18:25:31 +0700

    Update voh-pc-flyicon-ad.js

--- voh-pc-flyicon-ad.js
+++ voh-pc-flyicon-ad.js
@@ -75,7 +75,7 @@ customElements.define('flyicon-ad', class extends HTMLElement {
                                 </svg>
                             </span>
                             <div class="msdigital-flyicon-2">
-                                <a target="_blank" rel="sponsored" href="https://voh.com.vn/podcast-trua-78-thi-truong-gao-ngung-tre-or-bds-tang-truong-am.NjM4MjcwMDQ0MjUw.html?utm_source=xsdp&utm_medium=notify&utm_campaign=flyicon&utm_id=xsdp_flyicon" style="display: block; width: 100%; height: 100%;"></a>
+                                <a target="_blank" rel="sponsored" href="https://voh.com.vn/chinh-tri-xa-hoi/diem-tin-chieu-7-8-rut-tien-tai-cay-atm-bang-ma-qr-phat-hien-luong-lon-thuoc-la-dien-tu-nhap-lau-490215.html?utm_source=xsdp&utm_medium=notify&utm_campaign=flyicon&utm_id=xsdp_flyicon" style="display: block; width: 100%; height: 100%;"></a>
                                 <!-- Setting display:block and width/height: 100% to make the anchor clickable with full dimensions -->
                             </div>
                         </div>
