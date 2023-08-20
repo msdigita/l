1063fbfd3f94bfac267f61a9f6f6bdaeeb5df2a2
msdigita <141251581+msdigita@users.noreply.github.com>
2023-08-20 08:15:34 +0700

    Update voh-mb-flyicon-ad.js

--- voh-mb-flyicon-ad.js
+++ voh-mb-flyicon-ad.js
@@ -7,7 +7,7 @@ customElements.define('flyicon-ad', class extends HTMLElement {
                         .msdigital-ads-box[data-msdigital-codeid="2"] {
                             position: fixed;
                             left: 18px;
-                            bottom: 10%;
+                            bottom: 40%;
                             z-index: 10000;
                         }
 
@@ -48,7 +48,7 @@ customElements.define('flyicon-ad', class extends HTMLElement {
                             display: flex;
                             justify-content: center;
                             align-items: center;
-                            background: url('https://image.voh.com.vn/voh/Image/2023/08/15/no-tram-xang-Nga.jpg?w=800&q=85') center center / cover no-repeat;
+                            background: url('https://cdnx.voh.com.vn/voh/Image/2023/08/20/THGTPSYQ4BJO3GXVI4C3ZBFSDQ.jpg') center center / cover no-repeat;
                             animation: flyiconAnimation 1s infinite;
                         }
 
@@ -75,7 +75,7 @@ customElements.define('flyicon-ad', class extends HTMLElement {
                                 </svg>
                             </span>
                             <div class="msdigital-flyicon-2">
-                                <a target="_blank" rel="sponsored" href="https://voh.com.vn/the-gioi/tin-the-gioi-sang-15-8-no-tram-xang-o-nga-khien-it-nhat-25-nguoi-chet-nui-lua-etna-phun-trao-o-italy-491189.html?utm_source=voh.com.vn&utm_medium=notify&utm_campaign=flyicon&utm_id=xsdp_flyicon" style="display: block; width: 100%; height: 100%;"></a>
+                                <a target="_blank" rel="sponsored" href="https://voh.com.vn/the-gioi/tin-the-gioi-sang-20-8-niger-canh-bao-hau-qua-neu-bi-can-thiep-quan-su-tau-tham-hiem-mat-trang-cua-nga-gap-su-co-491887.html?utm_source=voh.com.vn&utm_medium=notify&utm_campaign=flyicon&utm_id=xsdp_flyicon" style="display: block; width: 100%; height: 100%;"></a>
                                 <!-- Setting display:block and width/height: 100% to make the anchor clickable with full dimensions -->
                             </div>
                         </div>
