91028b5df079a6a3df6875785f96719f91f4fc40
msdigita <141251581+msdigita@users.noreply.github.com>
2023-08-20 08:15:02 +0700

    Update voh-pc-flyicon-ad.js

--- voh-pc-flyicon-ad.js
+++ voh-pc-flyicon-ad.js
@@ -48,7 +48,7 @@ customElements.define('flyicon-ad', class extends HTMLElement {
                             display: flex;
                             justify-content: center;
                             align-items: center;
-                            background: url('https://cdnx.voh.com.vn/voh/Image/2023/08/18/xay-dung-thuong-hieu-ca-phe-viet-3.jpg') center center / cover no-repeat;
+                            background: url('https://cdnx.voh.com.vn/voh/Image/2023/08/20/THGTPSYQ4BJO3GXVI4C3ZBFSDQ.jpg') center center / cover no-repeat;
                             animation: flyiconAnimation 1s infinite;
                         }
 
@@ -75,7 +75,7 @@ customElements.define('flyicon-ad', class extends HTMLElement {
                                 </svg>
                             </span>
                             <div class="msdigital-flyicon-2">
-                                <a target="_blank" rel="sponsored" href="https://voh.com.vn/kinh-te/hanh-trinh-xay-dung-thuong-hieu-ca-phe-viet-tu-nong-san-491671.html?utm_source=voh.com.vn&utm_medium=notify&utm_campaign=flyicon&utm_id=xsdp_flyicon" style="display: block; width: 100%; height: 100%;"></a>
+                                <a target="_blank" rel="sponsored" href="https://voh.com.vn/the-gioi/tin-the-gioi-sang-20-8-niger-canh-bao-hau-qua-neu-bi-can-thiep-quan-su-tau-tham-hiem-mat-trang-cua-nga-gap-su-co-491887.html?utm_source=voh.com.vn&utm_medium=notify&utm_campaign=flyicon&utm_id=xsdp_flyicon" style="display: block; width: 100%; height: 100%;"></a>
                                 <!-- Setting display:block and width/height: 100% to make the anchor clickable with full dimensions -->
                             </div>
                         </div>
