618e56089f24a822766ca3a2680c5320edb3f182
msdigita <141251581+msdigita@users.noreply.github.com>
2023-10-27 18:32:37 +0700

    Update randomAd.js

--- randomAd.js
+++ randomAd.js
@@ -13,17 +13,35 @@
 
     // HTML
     var html = `
-        <div class="random-ad"><a href="https://voh.com.vn/benh/cach-phan-biet-benh-thuy-dau-va-dau-mua-khi-500029.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner" title="Đậu mùa khỉ" rel="sponsored" target="_blank"> <img src="https://i.pinimg.com/originals/af/3c/9d/af3c9d528ad73b7d78b2b3a5415b1f57.jpg"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/benh/nguon-goc-va-con-duong-lay-truyen-cua-benh-dau-mua-khi-500021.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner" title="Đậu mùa khỉ" rel="sponsored" target="_blank"> <img src="https://i.pinimg.com/originals/cb/cd/e7/cbcde7ad32059846e1c3c31757af275b.jpg"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/khoe/dau-hieu-dac-trung-cua-benh-dau-mua-khi-va-cach-phong-ngua-500127.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner" title="Đậu mùa khỉ" rel="sponsored" target="_blank"> <img src="https://i.pinimg.com/originals/67/85/f7/6785f7959df4299395305049456306bc.jpg"> </a></div>
+        <div class="random-ad">
+            <a href="https://voh.com.vn/show/tang-duy-tan-am-nhac-mot-mau-hay-khong-tuy-vao-nhin-nhan-cua-khan-gia-btv134-499423.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile" title="Tăng Duy Tân" rel="sponsored" target="_blank"> 
+                <img src=""> 
+            </a>
+        </div>
+
+        <div class="random-ad">
+            <a href="https://voh.com.vn/show/trinh-dinh-quang-tung-khong-dam-nghe-lai-nhac-cua-minh-vi-ap-luc-btv64-492046.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile" title="Trịnh Đình Quang" rel="sponsored" target="_blank"> 
+                <img src="https://i.pinimg.com/originals/ed/0f/69/ed0f69b967bec18236349e45eaf56237.jpg"> 
+            </a>
+        </div>
+
+        <div class="random-ad">
+            <a href="https://voh.com.vn/show/cao-thai-son-tu-hao-la-ca-si-8x-duy-nhat-canh-tranh-duoc-top-trending-voi-cac-genz-btv134-494696.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner" title="Cao Thái Sơn" rel="sponsored" target="_blank"> 
+                <img src="https://i.pinimg.com/originals/a3/f6/b9/a3f6b949717e4a92ca2a2be54e2a3d6a.jpg"> 
+            </a>
+        </div>
     `;
 
-    document.body.innerHTML += html;
+    // Chèn HTML vào thẻ <div id="xsdp-promote-voh">
+    var targetDiv = document.getElementById('xsdp-promote-voh');
+    if (targetDiv) {
+        targetDiv.innerHTML = html;
+    }
 
     // JavaScript logic
     window.onload = function() {
-        // Lấy tất cả phần tử có class là "random-ad"
-        var ads = document.querySelectorAll('.random-ad');
+        // Lấy tất cả phần tử có class là "random-ad" từ bên trong targetDiv
+        var ads = targetDiv.querySelectorAll('.random-ad');
 
         // Chọn ngẫu nhiên một quảng cáo
         var randomAdIndex = Math.floor(Math.random() * ads.length);
