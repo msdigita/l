388fa0ee6d3b049390a765f130dd73f754ba4801
msdigita <141251581+msdigita@users.noreply.github.com>
2023-11-06 14:15:51 +0700

    Update randomAd.js

--- randomAd.js
+++ randomAd.js
@@ -22,9 +22,10 @@
     document.head.appendChild(style);
 
     var html = `
-        <div class="random-ad"><a href="https://voh.com.vn/tin-sao/obito-cu-viet-va-khoc-cho-den-khi-album-danh-doi-hoan-thanh-btv134-500321.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner" title="Obito" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/SH0dJLQ.png"> </a></div>
-        <div class="random-ad"><a href="https://voh.com.vn/show/ly-tuan-kiet-tu-hao-la-mot-phan-cua-hkt-len-tieng-ve-moi-quan-he-giua-titi-va-ho-gia-hung-btv64-496495.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Lý Tuấn Kiệt" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/yrsbB5w.png"> </a></div>
-        <div class="random-ad"><a href="https://voh.com.vn/show/phuong-my-chi-trong-gioi-nghe-si-tien-bac-khong-phai-la-thu-quan-trong-nhat-btv134-498925.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Phương Mỹ Chi" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/rJsB0KK.png"> </a></div>
+        <div class="random-ad"><a href="https://voh.com.vn/tin-sao/vinh-rau-faptv-thua-nhan-cang-thang-khi-lam-viec-voi-huynh-phuong-btv64-486661.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Vinh Râu" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/g6cLO5e.png"> </a></div>
+        <div class="random-ad"><a href="https://voh.com.vn/tin-sao/thuy-kieu-faptv-7-nam-dong-hanh-cung-faptv-biet-ro-con-nguoi-that-huynh-phuong-btv134-486793.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Thúy Kiều" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/RF0fsdS.png"> </a></div>
+        <div class="random-ad"><a href="https://voh.com.vn/show/hau-truong-tet-o-lang-dia-nguc-quang-tuan-chay-mau-cam-vi-lanh-nguyen-thao-khong-dam-noi-chuyen-voi-vo-tan-phat-btv64-501432.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Dàn sao Tết Ở Làng Địa Ngục" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/8QTZKN6.png"> </a></div>
+        <div class="random-ad"><a href="https://voh.com.vn/show/giai-ma-moi-duyen-2-ty-giua-neko-le-va-ngoc-thanh-tam-btv134-501274.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Ngọc Thanh Tâm & Neko Lê" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/kvh8IgC.png"> </a></div>
 `;
     var targetDiv = document.getElementById('xsdp-promote-voh');
     if (targetDiv) {
