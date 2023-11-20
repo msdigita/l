3f2129c32fd0f2ba2d6c6a42c8c7cad1264f6997
msdigita <141251581+msdigita@users.noreply.github.com>
2023-11-20 14:16:18 +0700

    Update randomAd.js

--- randomAd.js
+++ randomAd.js
@@ -15,10 +15,9 @@
                     margin-top: 5px;
                 }
             </style>
-            <div class="random-ad"><a href="https://voh.com.vn/tin-sao/vinh-rau-faptv-thua-nhan-cang-thang-khi-lam-viec-voi-huynh-phuong-btv64-486661.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Vinh Râu" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/a6tlvGp.png"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/tin-sao/thuy-kieu-faptv-7-nam-dong-hanh-cung-faptv-biet-ro-con-nguoi-that-huynh-phuong-btv134-486793.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Thúy Kiều" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/fNKueEm.png"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/show/hau-truong-tet-o-lang-dia-nguc-quang-tuan-chay-mau-cam-vi-lanh-nguyen-thao-khong-dam-noi-chuyen-voi-vo-tan-phat-btv64-501432.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Dàn sao Tết Ở Làng Địa Ngục" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/8QTZKN6.png"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/show/giai-ma-moi-duyen-2-ty-giua-neko-le-va-ngoc-thanh-tam-btv134-501274.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Ngọc Thanh Tâm & Neko Lê" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/T6X8zgQ.png"> </a></div>  
+            <div class="random-ad"><a href="https://voh.com.vn/benh/vi-sao-goi-benh-cum-la-cum-mua-ac-tinh-503634.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Cúm mùa - cúm mùa ác tính" rel="sponsored" target="_blank"> <img src="https://imgur.com/a/U5ha5Ch.jpeg"> </a></div>
+<div class="random-ad"><a href="https://voh.com.vn/benh/nguyen-nhan-nao-khien-benh-cum-mua-gia-tang-503624.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Cúm mùa -  nguyên nhân bệnh gia tăng" rel="sponsored" target="_blank"> <img src="https://imgur.com/a/QV4GkiR.jpeg"> </a></div>
+<div class="random-ad"><a href="https://voh.com.vn/show/nguoi-vo-cuoi-cung-nhung-cau-chuyen-thu-vi-trong-qua-trinh-lam-phim-lan-dau-duoc-tiet-lo-502269.html?utm_source=xsdp&utm_medium=notify&utm_campaign=mobile_banner?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Dàn sao Người Vợ Cuối Cùng" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/5ov2j43.png"> </a></div>
     `;
 
     var targetDiv = document.getElementById('xsdp-promote-voh');
