f8e3a1acf5449e380e3c2d4d4325b6294159f263
msdigita <141251581+msdigita@users.noreply.github.com>
2024-01-04 18:19:49 +0700

    Update randomAd.js

--- randomAd.js
+++ randomAd.js
@@ -15,10 +15,12 @@
                     margin-top: 5px;
                 }
             </style>
-            <div class="random-ad"><a href="https://voh.com.vn/benh/cach-nhan-biet-nhiem-khuan-mycoplasma-508305.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Hội chứng phổi trắng - Cách nhận biết" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/tZYTvRU.jpeg"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/benh/con-duong-lay-truyen-va-bien-chung-khi-nhiem-mycoplasma-508376.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Hội chứng phổi trắng - Đường lây nhiễm" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/Ktqcmz0.jpeg"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/benh/corticoid-con-dao-hai-luoi-voi-benh-phoi-trang-508381.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Hội chứng phổi trắng - Thuốc Corticoid" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/9n7ThRA.jpeg"> </a></div>
-<div class="random-ad"><a href="https://voh.com.vn/benh/cach-dung-thuoc-va-phong-benh-phoi-trang-do-mycoplasma-508403.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Hội chứng phổi trắng - Phòng bệnh" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/5tWnQi4.jpeg"> </a></div>
+            <div class="random-ad"><a href="https://voh.com.vn/benh/vi-sao-nguoi-gay-khong-uong-ruou-bia-van-bi-gan-nhiem-mo-510601.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Gan nhiễm mỡ - Đối tượng" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/Pqyha1F.jpeg"> </a></div>
+<div class="random-ad"><a href="https://voh.com.vn/benh/khi-nao-can-cho-tre-kham-sang-loc-gan-nhiem-mo-510634.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Gan nhiễm mỡ - Trẻ em" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/OqxH1cx.jpeg"> </a></div>
+<div class="random-ad"><a href="http://voh.com.vn/benh/co-nen-an-chay-khi-bi-gan-nhiem-mo-510743.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Gan nhiễm mỡ - Ăn chay" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/dSA73uV.jpeg"> </a></div>
+<div class="random-ad"><a href="https://voh.com.vn/benh/gan-nhiem-mo-co-nguy-hiem-khong-cach-dieu-tri-phong-tranh-510610.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Gan nhiễm mỡ - Điều trị, phòng ngừa" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/YsqH8Ik.jpeg"> </a></div>
+<div class="random-ad"><a href="https://voh.com.vn/benh/nguoi-bi-gan-nhiem-mo-nen-an-gi-kieng-an-gi-510622.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Gan nhiễm mỡ - Dinh dưỡng" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/wWnvCNk.jpeg"> </a></div>
+<div class="random-ad"><a href="https://voh.com.vn/benh/che-do-tap-luyen-de-phong-tranh-va-cai-thien-gan-nhiem-mo-510720.html?utm_source=xsdp&utm_medium=&utm_campaign=mobile_banner" title="Gan nhiễm mỡ - Luyện tập" rel="sponsored" target="_blank"> <img src="https://i.imgur.com/0glsKil.jpeg"> </a></div>
 `;
 
     var targetDiv = document.getElementById('xsdp-promote-voh');
